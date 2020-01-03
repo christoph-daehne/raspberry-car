173c009b2c6161184f30e833121a5e0e5852a9d5
Christoph Dähne <christoph.daehne@sandstorm.de>
2020-01-03 02:44:38 +0100

    sending events to server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/controller.html');
+});
+
+io.on('connection', socket => {
+    console.log('something connected');
+    socket.on('command', cmd => console.log('command: ', cmd));
+    socket.on('disconnect', () => console.log('something disconnected'));
 });
 
 http.listen(3000, () => {
